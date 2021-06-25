95d584c572e1ad3a8cad80ba83ddb7d8b6da7e1b
Pedro Verani <pedropverani@hotmail.com>
2021-06-25 11:09:01 -0300

    removing log

--- app/helper/db/connection.js
+++ app/helper/db/connection.js
@@ -2,7 +2,6 @@ const Mongoose = require('mongoose');
 
 /* istanbul ignore next */
 const URL = process.env.NODE_ENV === 'test' ? process.env.URL_TEST : process.env.URL;
-console.log(process.env.NODE_ENV === 'test', 'node_env');
 
 const connectDatabase = () => {
     const connectionOptions = { reconnectTries: Number.MAX_VALUE,reconnectInterval: 500, useNewUrlParser: true };
